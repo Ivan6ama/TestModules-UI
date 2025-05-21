2349dadafb8d7f7aaa0672923a800858a9cb73c4
Ivan <ivan6ama9@gmail.com>
2025-05-21 10:46:14 -0300

    Primera version de la interfaz de usuario con comunicacion mediante puerto serie

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,10 +6,205 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    serialPort = new QSerialPort(this);
+
+    connect(serialPort,&QSerialPort::readyRead,this,&MainWindow::on_serialPort_Rx);
+
+    ui->comboBoxPorts->addItem("COM5");
+    ui->comboBoxPorts->addItem("COM3");
+    ui->comboBoxPorts->addItem("COM9");
+    ui->comboBoxPorts->addItem("COM12");
+
+    ui->buttonOpenPort->setText("OPEN");
+    ui->buttonSendSerial->setText("SEN");
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_buttonOpenPort_clicked()
+{
+    if(serialPort->isOpen()){
+        serialPort->close();
+        ui->buttonOpenPort->setText("OPEN");
+    }else{
+        serialPort->setPortName(ui->comboBoxPorts->currentText());
+        serialPort->setBaudRate(QSerialPort::Baud115200);
+        if(serialPort->open(QSerialPort::ReadWrite)){
+            ui->buttonOpenPort->setText("CLOSE");
+        }
+    }
+}
+
+
+void MainWindow::on_buttonSendSerial_clicked()
+{
+    uint8_t id;
+
+    id = ALIVE;
+    send_Data_Serial(id);
+}
+
+void MainWindow::on_serialPort_Rx()
+{
+    unsigned char *incomingBuffer;
+    int count;
+
+    count = serialPort->bytesAvailable();
+
+    if(count<=0)
+        return;
+
+    incomingBuffer = new unsigned char[count];
+
+    serialPort->read((char *)incomingBuffer,count);
+
+    for(int i =0 ; i < count; i++){
+        switch (protocolState ){
+        case HEADER_U:
+            if (incomingBuffer[i] == 'U'){
+                protocolState = HEADER_N;
+            }
+            break;
+        case HEADER_N:
+            if (incomingBuffer[i] == 'N')
+                protocolState = HEADER_E;
+            else{
+                i--;
+                protocolState = HEADER_U;
+            }
+            break;
+        case HEADER_E:
+            if (incomingBuffer[i] == 'E')
+                protocolState = HEADER_R;
+            else{
+                i--;
+                protocolState = HEADER_U;
+            }
+            break;
+        case HEADER_R:
+            if (incomingBuffer[i] == 'R')
+                protocolState = NBYTES;
+            else{
+                i--;
+                protocolState = HEADER_U;
+            }
+            break;
+        case NBYTES:
+            rxData.nBytes = incomingBuffer[i];
+            protocolState = TOKEN;
+            break;
+        case TOKEN:
+            if (incomingBuffer[i] == ':'){
+                protocolState = PAYLOAD;
+                rxData.cheksum = 'U'^'N'^'E'^'R'^ rxData.nBytes^':';
+                rxData.payLoad[0] = rxData.nBytes;
+                rxData.index = 1;
+            }
+            else{
+                i--;
+                protocolState = HEADER_U;
+            }
+            break;
+        case PAYLOAD:
+            if (rxData.nBytes > 1){
+                rxData.payLoad[rxData.index++] = incomingBuffer[i];
+                rxData.cheksum ^= incomingBuffer[i];
+            }
+            rxData.nBytes--;
+            if(rxData.nBytes == 0){
+                protocolState = HEADER_U;
+                if(rxData.cheksum == incomingBuffer[i]){
+                    decode_Payload(&rxData.payLoad[0]);
+                }
+            }
+            break;
+        default:
+            protocolState = HEADER_U;
+            break;
+        }
+    }
+    delete [] incomingBuffer;
+}
+
+void MainWindow::send_Data_Serial(uint8_t ID)
+{
+    uint8_t bufTx[SIZEBUFRX], index, chks;
+
+    index = 0;
+    chks = 0;
+
+    bufTx[index++] = 'U';
+    bufTx[index++] = 'N';
+    bufTx[index++] = 'E';
+    bufTx[index++] = 'R';
+    bufTx[index++] = 0x00;
+    bufTx[index++] = ':';
+
+    if(serialPort->isOpen()){
+        switch(ID){
+        case ALIVE:
+            ui->plainTextTX->appendPlainText(">> Estas vivo ???");
+
+            bufTx[index++] = ALIVE;
+
+            bufTx[NBYTES] = 2;
+            break;
+        default:
+            break;
+        }
+
+        for(int a = 0 ; a < index ; a++){
+            chks ^= bufTx[a];
+        }
+
+        bufTx[index] = chks;
+
+        /*Mostrar los datos enviados en formato hexadecimal*/
+        QString hexString;
+        for(int i = 0; i < bufTx[NBYTES] + PAYLOAD; i++) {
+            hexString += QString("%1 ").arg(bufTx[i], 2, 16, QChar('0')).toUpper();
+        }
+        ui->plainTextTX->appendPlainText("Enviando: " + hexString.trimmed());
+
+        /*Envio de los datos al puerto serie*/
+        if(serialPort->isWritable()){
+            serialPort->write(reinterpret_cast<char *>(bufTx),bufTx[NBYTES] + PAYLOAD);
+        }
+
+        writeFlag = true;
+    }
+}
+
+void MainWindow::decode_Payload(uint8_t *buf)
+{
+    switch (buf[1]){
+    case ALIVE:
+        ui->plainTextRX->appendPlainText("Estoy vivo");
+        break;
+    /*case ASENSORS:
+        w.ui8[0] = buf[2];
+        w.ui8[1] = buf[3];
+        ui->plainTextRX->appendPlainText("Sensor Izquierda:" + QString("%1").arg(w.ui16[0], 2, 10, QChar('0')));
+
+        w.ui8[0] = buf[4];
+        w.ui8[1] = buf[5];
+        ui->plainTextRX->appendPlainText("Sensor Central:" + QString("%1").arg(w.ui16[0], 2, 10, QChar('0')));
+
+        w.ui8[0] = buf[6];
+        w.ui8[1] = buf[7];
+        ui->plainTextRX->appendPlainText("Sensor Derecha:" + QString("%1").arg(w.ui16[0], 2, 10, QChar('0')));
+        break;*/
+    default:
+        w.ui8[0] = buf[2];
+        w.ui8[1] = buf[3];
+        ui->plainTextRX->appendPlainText("Sensor Izquierda:" + QString("%1").arg(w.ui16[0], 2, 10, QChar('0')));
+        ui->plainTextRX->appendPlainText("COMANDO DESCONOCIDO");
+        break;
+    }
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,10 +2,45 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtSerialPort/QSerialPort>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
+
+#define     SIZEBUFRX   256
+#define     SIZEBUFTX   256
+
+typedef enum{
+    ALIVE           = 0xF0,
+}_eCMD;
+
+typedef enum{
+    HEADER_U,
+    HEADER_N,
+    HEADER_E,
+    HEADER_R,
+    NBYTES,
+    TOKEN,
+    PAYLOAD
+}_eProtocol;
+
+typedef union{
+    uint8_t     ui8[4];
+    int8_t      i8[4];
+    uint16_t    ui16[2];
+    int16_t     i16[2];
+    uint32_t    ui32;
+    int32_t     i32;
+}_uWork;
+
+typedef struct{
+    uint8_t timeOut;
+    uint8_t cheksum;
+    uint8_t payLoad[256];
+    uint8_t nBytes;
+    uint8_t index;
+}_sData;
 
 class MainWindow : public QMainWindow
 {
@@ -15,7 +50,26 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_buttonOpenPort_clicked();
+    void on_buttonSendSerial_clicked();
+
+    void on_serialPort_Rx();
+    void send_Data_Serial(uint8_t ID);
+    void decode_Payload(uint8_t *buf);
+
 private:
     Ui::MainWindow *ui;
+
+    /* General purpose variables ----------------------------------------------------------------------*/
+    _uWork                  w;
+
+
+    /* Serial communication variables -----------------------------------------------------------------*/
+    QSerialPort             *serialPort;
+    _sData                  rxData;
+    _eCMD                   ID;
+    _eProtocol              protocolState;
+    uint8_t                 writeFlag;
 };
 #endif // MAINWINDOW_H
